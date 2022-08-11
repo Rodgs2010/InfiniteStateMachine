381479601748380ab74fbc7ce4dda5c28c4ab293
Rodrigo Alves Bolincenha <65686723+Rodgs2010@users.noreply.github.com>
2022-08-11 13:41:40 -0300

    Update StateMachine.java

--- StateMachine.java
+++ StateMachine.java
@@ -1,3 +1,33 @@
+/*
+  Rodrigo Alves Bolincenha BCC 4 periodo noite
+https://github.com/Rodgs2010/MaquinaDeEstadoFinito
+    
+  Este programa, quando executado, irá determinar se uma string de 
+  entrada faz parte da linguagem 𝐿 definida por 𝐿 = {𝑥 | 𝑥 ∈{𝑎, 𝑏} 
+  𝑒 𝑐𝑎𝑑𝑎 𝑎 𝑒𝑚 𝑥 é 𝑠𝑒𝑔𝑢𝑖𝑑𝑜 𝑝𝑜𝑟 𝑝𝑒𝑙𝑜 𝑚𝑒𝑛𝑜𝑠 𝑑𝑜𝑖𝑠 𝑏} segundo o alfabeto Σ = {𝑎,𝑏, 𝑐}.
+
+  Este programa le 4 arquivos de texto contendo uma quantidade exata de Strings definido na primeiro linha de cada 
+  arquivo.
+  Exemplo:
+  string1.txt    string2.txt
+  3               4
+  abbaba         abbbbbbbbbabb
+  abababb        aa
+  bbabbaaab      bb
+                 acc
+
+Obs: Os arquivos de texto estao dentro de uma pasta chamada "arquivosDeTexto", portanto a criaçao de um novo arquivo de texto deve ser dentro dessa pasta e também seguir o padrao dos arquivos existentes mostrado no exemplo acima.
+
+na funcao abrirArquivo(), se for criar uma nova pasta com novos arquivos de texto dentro, por favor colocar
+dentro do metodo Paths.get("caminho da pasta com os arquivos dentro");
+
+Exemplo de como esta no meu computador
+path = Paths.get("C:\\Users\\Rodrigo\\IdeaProjects\\PARA-CONCU-DISTRI\\src\\arquivosDeTexto");
+Exemplo de como esta no replit.com
+path = Paths.get("arquivosDeTexto");
+*/
+
+// importando as bibliotecas para abrir, let e fechar arquivos junto com as suas exceçoes.
 package maquina;
 
 import java.nio.file.Files;
@@ -15,7 +45,8 @@ public class StateMachine {
     private static int i = -1;
     private static int tamanhoString;
     private static char let;
-
+    
+//funcao para abrir o arquivo
     public static void abrirArquivo(){
         System.out.println("Coloque o nome do arquivo ou diretorio:");
         path = Paths.get("C:\\Users\\Rodrigo\\IdeaProjects\\PARA-CONCU-DISTRI\\src\\arquivosDeTexto");
@@ -25,12 +56,20 @@ public class StateMachine {
             System.out.printf("%s does not exist%n", path);
         }
     }
-
+  //funcao para ler o Arquivo
     public static void lerArquivo(){
         try{
             if(Files.isDirectory(path) || Files.isReadable(path)){
+                 // Cria um objeto do tipo Path que contem o caminho da pasta com os arquivos dentro
 
                 DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
+                /* 
+                  Um loop para percorrer os arquivos de texto dentro da pasta.
+                  Mostra o nome do primeiro arquivo lido.
+                  o While percorra o conteudo dentro de cada.
+                  Se o numero de strings fornecido dentro do arquivo estiver errado
+                  sera lancado um erro e o arquivo sera fechado na hora.
+                */
                 for(Path p : directoryStream){
                     System.out.println("Lendo arquivo: " + p.getFileName());
                     input = new Scanner(Paths.get(String.valueOf(p)));
@@ -38,6 +77,7 @@ public class StateMachine {
                         int numString = input.nextInt();
                         String[] texto = new String[numString];
                         System.out.println("numero de Strings no arquivo: " + numString);
+                        // pega o numero de Strings que foi fornecido na primeira linha do arquivo
                         for(int j = 0; j < numString; j++){
                             texto[j] = input.next();
                             tamanhoString = texto[j].length();
@@ -54,12 +94,16 @@ public class StateMachine {
             e.printStackTrace();
         }
     }
+    //funcao para fechar o arquivo
     public static void fecharArquivo(){
         if(input != null){
             input.close();
             System.out.println("Fechando: " + path.getFileName());
         }
     }
+    /*
+      A maquina tem o start e os estados s1, s2, s3(vericacao), s4.
+    */
     public static char getString(String s){
         i++;
         return s.charAt(i);
